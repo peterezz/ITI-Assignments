5a5ac83613b9175784cfbb8192690c6ad4711124
Peter Ezzat <peterezzat97@gmail.com>
2023-01-24 21:52:28 +0200

    Solution to assignment of day3

---
+++ Day3/Day3/Operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public static class Operations
+    {
+        internal static double Devide(int firstNumber, int secondNumber)
+        {
+            if (secondNumber != 0)
+                return firstNumber / secondNumber;
+            else return 0;
+        }
+
+        internal static int Product(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        internal static int Subtract(int firstNumber, int secondNumber)
+        {
+            return   firstNumber - secondNumber;
+        }
+
+        internal static int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
