0a45359fb703dde896f1f69d49b171e456a647d9
Peter Ezzat <peterezzat97@gmail.com>
2023-03-07 15:09:32 +0200

    Sol to assignment day2 of advanced c# course note: this answer is too much compared to the assignment requirement

---
+++ Day2/LongestDistanceBetweenTwoEqualCells.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class LongestDistanceBetweenTwoEqualCells
+    {
+        int[] array;
+        
+        public LongestDistanceBetweenTwoEqualCells(string inputData)
+        {
+            string[] userInputArray = inputData.Split(' ');
+            array = new int[userInputArray.Length];
+            for(int i = 0; i < userInputArray.Length; i++)
+            {
+                array[i] = int.Parse(userInputArray[i]);
+            }
+        }
+        public List<ResultSet> FindLongestDistance()
+        {
+            List<ResultSet> resultSet = new List<ResultSet>();
+
+
+            for (int row = 0; row < array.Length; row++)
+            {
+                ResultSet? Searchresult = resultSet.Find(x => x.item == array[row]);
+                if (Searchresult == null)
+                {
+                    resultSet.Add(new ResultSet
+                    {
+                        item = array[row],
+                        initIndex = row,
+                        lastIndex = Array.LastIndexOf(array, array[row])
+                    });                
+                }
+            }
+            return resultSet;
+        }
+        public void SearchArray(int key)
+        {
+            int FirstOccureance = Array.IndexOf(array,key);
+            int LastOccureance = Array.LastIndexOf(array,key);
+            if(FirstOccureance == -1)
+                Console.WriteLine($"number: {key} not found.");
+            else
+                Console.WriteLine($"distance between the occureance of number:" +
+                    $" {key} is: {LastOccureance - FirstOccureance -1}");      
+
+
+
+        }
+    }
+}
---
+++ Day2/NICSingltonClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public enum Type
+    {
+        Ethernet,
+        TokenRing
+    }
+    internal class NICSingltonClass
+    {
+        string manufactureDate;
+        string macAddress;
+         Type type;
+
+
+        private static NICSingltonClass instance;
+
+        public NICSingltonClass(string manufactureDate, string macAddress, Type type)
+        {
+            this.manufactureDate = manufactureDate;
+            this.macAddress = macAddress;
+            this.type = type;
+        }
+
+        public static NICSingltonClass CreateNicCard()
+        {
+            if(instance == null)
+                instance = new NICSingltonClass( "assasa",  "dsdsds",  Type.Ethernet);
+
+            return instance;
+        }
+        
+
+
+        
+        
+
+        
+    }
+}
