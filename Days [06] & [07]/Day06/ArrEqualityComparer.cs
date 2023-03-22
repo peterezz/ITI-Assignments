bc1fe10b5c69ff2e38c70798f6360a9a8fbc6b8b
Peter Ezzat <peterezzat97@gmail.com>
2023-03-22 12:06:59 +0200

    Answer to days [06] & [07] assignments (LINQ Assignment)

---
+++ Days [06] & [07]/Day06/ArrEqualityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class ArrEqualityComparer : IEqualityComparer<string>
+{
+
+    public bool Equals(string? x, string? y)
+    {
+        string resultx = String.Join("", x.ToLower().OrderBy(chr => chr));
+        string resulty = String.Join("", y.ToLower().OrderBy(chr => chr));
+        return resultx == resulty;
+    }
+    public int GetHashCode([DisallowNull] string obj)
+    {
+
+        return base.GetHashCode();
+    }
+   
+}
---
+++ Days [06] & [07]/Day06/IgnoreUpperCaseComparer.cs
@@ -0,0 +1,7 @@
+﻿internal class IgnoreUpperCaseComparer : IComparer<string>
+{
+    public int Compare(string? x, string? y)
+    {
+        return x.ToLower().CompareTo(y.ToLower());
+    }
+}
