9749982c813ea3cd6aeda56193ba0637c1ee3490
Peter Ezzat <peterezzat97@gmail.com>
2023-04-07 00:40:24 +0200

    sol to assignment day 7 MVC Course

---
+++ MVC_Day7/MVC_Day7/Controllers/CarsController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Day7.Models;
+
+namespace MVC_Day7.Controllers
+{
+    public class CarsController : Controller
+    {
+        CarContext carContext = new CarContext( );
+        // GET: CarsController
+        public ActionResult Index( )
+        {
+            ViewBag.List = carContext.GetAllCars( );
+            return View( ViewBag.List );
+        }
+
+        // GET: CarsController/Details/5
+        public ActionResult Details( int id )
+        {
+            ViewBag.CarDetails = carContext.SelectCarById( id );
+            return View( ViewBag.CarDetails );
+        }
+
+        // GET: CarsController/Create
+        public ActionResult Create( )
+        {
+            return View( );
+        }
+
+        // POST: CarsController/Create
+        [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public ActionResult Create( Car car )
+        {
+            try
+            {
+                carContext.CreateNewCar( car );
+                return RedirectToAction( nameof( Index ) );
+            }
+            catch
+            {
+                return View( );
+            }
+        }
+
+        // GET: CarsController/Edit/5
+        public ActionResult Edit( int? carNum )
+        {
+            var Car = carContext.SelectCarById( carNum ?? 0 );
+            return View( Car );
+        }
+
+        // POST: CarsController/Edit/5
+        [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public ActionResult Edit( Car car )
+        {
+            try
+            {
+                carContext.UpdateCar( car );
+                return RedirectToAction( nameof( Index ) );
+            }
+            catch
+            {
+                return View( );
+            }
+        }
+
+        // GET: CarsController/Delete/5
+        public ActionResult Delete( int carNum )
+        {
+            carContext.DeleteCar( carNum );
+            return RedirectToAction( nameof( Index ) );
+        }
+
+
+    }
+}
---
+++ MVC_Day7/MVC_Day7/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Day7.Models
+{
+    public class Car
+    {
+        public int Num { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        [Display( Name = "Manfacture" )]
+        public string Manfacture { get; set; }
+    }
+}
